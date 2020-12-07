8d789b040e0e4e464915cfa1d8f7a9464c095b30
alisoltanirad <soltaniradali@gmail.com>
2020-12-07 19:19:37 +0330

    Debug

--- character/image.py
+++ character/image.py
@@ -1,21 +1,22 @@
 import PIL.Image
 
+
 class Image:
-    characters = r'X@S8&$#%?x=*+!o;:,. '
+    characters = r'B8&WM#YXQO{}[]()I1i!pao;:,.    '
     n = len(characters)
 
     def __init__(self, path):
         self.__file = PIL.Image.open(path).convert('L')
-        self.__ascii = self.__to_ascii(self.__file)
+        self.__ascii = self.__to_ascii(self.__resize())
 
     def __str__(self):
         return self.__to_ascii(self.__resize())
 
-    def save(self):
-        with open('ascii_image.txt', 'w') as f:
+    def save(self, path='ascii_image.txt'):
+        with open(path, 'w') as f:
             f.write(self.__ascii)
 
-    def __resize(self, new_width=80):
+    def __resize(self, new_width=120):
         width, height = self.__file.size
         new_height = int(new_width * (height / width))
         return self.__file.resize((new_width, new_height))
@@ -25,7 +26,7 @@ class Image:
         ascii_image = ''
         for h in range(0, height):
             for w in range(0, width):
-                pixel = self.__file.getpixel((w,h))
+                pixel = image.getpixel((w,h))
                 ascii_image += self.characters[int(((self.n - 1) * pixel) / 256)]
             ascii_image += '\n'
         return ascii_image
